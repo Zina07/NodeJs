bc2a6911f1b51c72edec97f8a85a7cdfc37a26ab
zina <keiraayari@gmail.com>
2020-11-03 16:52:03 +0100

    rajout simple_form

--- express_simple_form/index.js
+++ express_simple_form/index.js
@@ -1,16 +1,57 @@
-const port = 3000;
+var express = require('express');
+var exphbs  = require('express-handlebars');
+ 
+const port = 3001;
+var app = express();
 
 
+app.engine('handlebars', exphbs({
+    defaultLayout: false,
+    layoutsDir: __dirname + "views/"
+}));
+  
+  app.set('view engine', 'handlebars');
 
-app.get('/hello', (req, res) => {
-    res.send('Hello world!');
+  // parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// parse application/json
+app.use(express.json());
+
+
+app.post('/form/studentsList', (req, res) => {
+  console.log('form parameter', req.body.username); 
 });
-app.get('/hola', (req, res) => {
-    res.send('Hola todos !');
-});
+  
+const students = ['Jean', 'Binta', 'Agathe', 'Adil'];
+
+  app.get('/handlebars', (req, res) => {
+    console.log('req', students)
+    res.render('home', {
+      body: (req.params.name)
+    })
+  });
+
+    //const newstudents = students.map.username ... à finir ....//
+
+  app.post('/handlebars', (req, res) => {
+    console.log('req', newstudents)
+    res.render('home', {
+      body: (req.params.name)
+    })
+  });
 
 
 
 app.listen(port, () => {
     console.log('Server started on port: ' + port);
   });
+
+
+
+
+  
+/*app.get('/handlebars', (req, res) => {
+    res.render('home', {
+        body: 'Welcome to express simple form !!!',
+    });
+});*/
